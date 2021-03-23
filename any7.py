01249eeda6307bb93ad81e672b8c058d475ead6b
whereislima <reach.lima@gmail.com>
2021-03-22 18:25:30 -0700

    first commit

---
+++ any7.py
@@ -0,0 +1,14 @@
+def any7(nums):
+    """Are any of these numbers a 7? (True/False)"""
+
+    # YOUR CODE HERE 
+    target = 7;
+    # iterate over array if any elements are 7
+    for num in nums:
+        if num == target:
+            return True
+    return False
+
+print("should be true", any7([1, 2, 7, 4, 5]))
+print("should be false", any7([1, 2, 4, 5]))
+
